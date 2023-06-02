02f5c9e6bd9b870974c86c95890802ba8fa03efa
karol <karolina.majkowska@o2.pl>
2023-06-02 13:56:17 +0200

    sketch of project

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -1,8 +1,21 @@
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QLabel
 from PyQt6.QtCore import QCoreApplication, QObject
 from PyQt6.QtCore import pyqtSlot 
+from PyQt6.QtSql import QSqlDatabase, QSqlQuery
 import sys
 from GUI_notes import NotesMainWindow
+import importlib.util
+
+# Ścieżka do pliku notes.py
+database_path = r'C:\Users\karol\OneDrive\Desktop\projekt\database\database.py'
+
+# Załaduj moduł notes
+database_spec = importlib.util.spec_from_file_location('databse', database_path)
+database_module = importlib.util.module_from_spec(database_spec)
+database_spec.loader.exec_module(database_module)
+
+# Importuj klasę Note
+NoteDatabase = database_module.NotesDatabase
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -22,16 +35,16 @@ class MainWindow(QMainWindow):
         self.centralWidget.setLayout(self.layout)
 
         self.mainLabel = QLabel()
-        self.mainLabel.setText("Welcome to our app, which manage your notes! :) ")
+        self.mainLabel.setText("Welcome to our app, which manage your notes! 🐸 ")
 
-        self.buttonOpenNotes = QPushButton("Open notes", self)
+        self.buttonOpenNotes = QPushButton("Open notes 🐈", self)
         self.buttonOpenNotes.clicked.connect(self.openNotes)
 
-        self.buttonNewNotes = QPushButton("New notes", self)
+        self.buttonNewNotes = QPushButton("New notes 🐧", self)
         self.buttonNewNotes.clicked.connect(self.openNotes)
 
-        self.buttonOpenNotesFromArchive = QPushButton("Open notes from archive", self)
-        self.buttonOpenNotesFromArchive.clicked.connect(self.openNotes)
+        self.buttonOpenNotesFromArchive = QPushButton("Open notes from archive 🦜", self)
+        self.buttonOpenNotesFromArchive.clicked.connect(self.open_database)
 
         self.layout.addWidget(self.mainLabel)
         self.layout.addWidget(self.buttonOpenNotes)
@@ -43,6 +56,22 @@ class MainWindow(QMainWindow):
         if self.notesWindow is None:  # Sprawdzenie, czy obiekt NotesMainWindow nie został już utworzony
             self.notesWindow = NotesMainWindow()
         self.notesWindow.show()
+
+    def open_database(self):
+        self.database = QSqlDatabase.addDatabase("QSQLITE")
+        self.database.setDatabaseName('NotesDatabase')
+
+        if not self.database.open():
+            self.result_label.setText("Nie udało się otworzyć bazy danych.")
+            return False
+
+        return True
+    
+
+    def close_database(self):
+        self.database.close()
+
+
 def main():
     app = QApplication(sys.argv)
     mainWindow = MainWindow()
--- notes/notes.py
+++ notes/notes.py
@@ -5,6 +5,7 @@ class Note():
         self.__title : str = ""
         self.__text : str = ""
         self.__time : datetime = ""
+        self.__active : bool = True
 
     def __str__(self):
         return self.__text + " " + str(self.__time)
@@ -20,3 +21,22 @@ class Note():
 
     def getText(self):
         return self.__text
+    
+    def setTitle(self, title):
+        self.__title = title
+        
+    def getTitle(self):
+        return self.__text
+        
+    def setActive(self, active):
+        self.__active = active
+
+    def getActive(self):
+        return self.__active
+    
+    def openedNote(self, title, text, time, active):
+        self.__title = title
+        self.__text = text
+        self.__time = time
+        self.__active = active
+
