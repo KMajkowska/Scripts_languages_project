7514fca94f7ac6711ab889a323a1d87e3114e15b
KMajkowska <96021599+KMajkowska@users.noreply.github.com>
2023-06-14 10:44:22 +0200

    this path work

--- GUI/functions.py
+++ GUI/functions.py
@@ -3,8 +3,14 @@ import speech_recognition as sr
 import emoji
 from pathlib import Path 
 
-CURRENT_NOTES_PATH = Path(__file__).resolve().parent.parent.parent.parent / 'notes/notes.py'
-
+if os.path.basename(sys.argv[0]) == "GUI.exe":
+    # Kod uruchomiony jako plik wykonywalny (.exe)
+    CURRENT_NOTES_PATH = Path(__file__).resolve().parent.parent.parent.parent / 'notes/notes.py'
+else:
+    # Kod uruchomiony z Visual Studio Code lub innego kontekstu
+    CURRENT_NOTES_PATH = os.path.join(os.getcwd(),"notes\\notes.py")
+    
+    
 def speech_to_text():
     r = sr.Recognizer()
     r.energy_threshold=4000 #Ustawiamy żeby mikrofon był bardziej czuły na wykrycie ciszy
